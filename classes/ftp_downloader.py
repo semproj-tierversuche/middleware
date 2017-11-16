db4609d8c63322cb75258bd64097260bec3481fd
bitPogo <geislemx@informatik.hu-berlin.de>
2017-11-16 23:25:27 +0100

    jetzt sind die Styleänderungen drin

--- classes/ftp_downloader.py
+++ classes/ftp_downloader.py
@@ -11,7 +11,7 @@ class ftpBasicDownloader:
 #    _ReturnAsDOM = False
 #    _Base = ''
 #    _Dirs = ''
-    __Con = ''
+    __Connectionnection= ''
 #    User = ''
 #    PW = ''
     __IsActiv = False
@@ -21,63 +21,63 @@ class ftpBasicDownloader:
         self._ResturnAsDOM = bool(ReturnAsDOM)
         self.User = ''
         self.PW = ''
-        self.__Con = ''
+        self.__Connection = ''
 
     def initializeConnection(self, WithTLS):
         #try:
         if True == bool(WithTLS):
-            self.__Con = FTP_TLS(self._Base)
+            self.__Connection = FTP_TLS(self._Base)
         else:
-            self.__Con = FTP(self._Base)
+            self.__Connection = FTP(self._Base)
         #except ftplib.all_errors as e:
         self._IsActiv = True
 
         #folgendes solltes später geloggt werden
         #try:
         if not self.User:
-            self.__Con.login()
+            self.__Connection.login()
         else:
-            self.__Con.login(user = self.User, passwd = self.PW)
+            self.__Connection.login(user = self.User, passwd = self.PW)
         #except ftplib.all_errors as e:
 
     def getFileList(self, Dir):
         Return = []
 
 #       try:
-        self.__Con.cwd(Dir)
+        self.__Connection.cwd(Dir)
         #except ftplib.all_errors as e
             #dosmt
-        self.__Con.retrlines('LIST', Return.append)
+        self.__Connection.retrlines('LIST', Return.append)
 
 #       try:
-        self.__Con.cwd('/')
+        self.__Connection.cwd('/')
         #except ftplib.all_errors as e
         return Return
 
     def changeDir(self,Dir):
         #try:
-        self.__Con.cwd(Dir)
+        self.__Connection.cwd(Dir)
         #except ftplib.all_errors as e
         #dosmt
 
     def goBackToBaseDir(self):
         #try:
-            self.__Con.cwd('/')
+            self.__Connection.cwd('/')
         #except ftplib.all_errors as e
         #dosmt
 
     def downloadFile(self, FileName, OutputFile):
        with open(OutputFile, 'wb') as WriteInto:
-            def push(block):
-                WriteInto.write(block)
+            def push(Block):
+                WriteInto.write(Block)
             #try:
-            self.__Con.retrbinary("RETR " + FileName, push)
+            self.__Connection.retrbinary("RETR " + FileName, push)
             #except ftplib.all_errors as e
 
 #Schliessen der Verbindung nicht vergessen
     def __del__(self):
         if True == self.__IsActiv:
             try:
-                self.__Con.quit()
+                self.__Connection.quit()
             except:
-                self.__Con.close()
+                self.__Connection.close()
