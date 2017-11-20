cb02e5f9afcee8198846f065fa6b5a83cace5cc2
bitPogo <geislemx@informatik.hu-berlin.de>
2017-11-20 15:12:29 +0100

    BugFix config & resource_download config.py -> vereinfachung der rules

--- __init__.py
+++ __init__.py
@@ -8,20 +8,50 @@ LastUpdate = 0
 
 #step 1 -> reading config
 Config = ConfigReader()
-Config.parseConfigFile("../config.xml")
+Config.parseConfigFile('../config.xml')
+print(Config._Resources)
 #step 2 -> prepare download
-download = ResourceDownloader()
-for resource in Config._Resources:
-    download.setBaseAddress(resource['address'])
+Download = ResourceDownloader()
+for Resource in Config._Resources:
+    Download.setBaseAddress(Resource['address'])
 #step 3 -> when we did the last update for this source
 #TODO
 #step 4 -> adding the sources
-    for subfolder in resource["folders"]:
-        if not bool(subfoldet['onInitializion']) or 0 == LastUpdate:
-            download.addSubFolder(subfolder)
+    for Subfolder in Resource['folders']:
+        if not bool(Subfolder['onInitializion']) or 0 == LastUpdate:
+            Download.addSubFolder(Subfolder)
 
 #step 5(optional) -> check md5 and remove invalide files
-    if bool(resource["md5"]):
-        download.checkMD5()
+    if bool(Resource['md5']):
+        Download.checkMD5()
+
+
+
+#step 6 -> set exclude and include rules by the config
+    for Exclusion in Resource['exclusions']:
+        for End in Exclusion['ending']:
+            Download.filterFiles(End, Download.FILTER_FILE_EXCLUDE_ENDS_WITH)
+        for Contains in Exclusion['contains']:
+            Download.filterFiles(Contains, Download.FILTER_FILE_EXCLUDE_CONTAINS)
+        for Pattern in Exclusion['pattern']:
+            Download.filterFiles(Pattern, Download.FILTER_FILE_EXCLUDE_PATTERN)
+        for StartDate in Exclusion['start_date']:
+            Download.filterFiles(StartDate, Download.FILTER_FILE_EXCLUDE_START_DATE)
+        for EndDate in Exclusion['end_date']:
+            Download.filterFiles(EndDate, Download.FILTER_FILE_EXCLUDE_END_DATE)
+
+    for Inclusion in Resource['inclusions']:
+        for End in Inclusion['ending']:
+            Download.filterFiles(End, Download.FILTER_FILE_INCLUDE_ENDS_WITH)
+        for Contains in Inclusion['contains']:
+            Download.filterFiles(Contains, Download.FILTER_FILE_INCLUDE_CONTAINS)
+        for Pattern in Inclusion['pattern']:
+            Download.filterFiles(Pattern, Download.FILTER_FILE_INCLUDE_PATTERN)
+        for StartDate in Inclusion['start_date']:
+            Download.filterFiles(StartDate, Download.FILTER_FILE_INCLUDE_START_DATE)
+        for EndDate in Inclusion['end_date']:
+            Download.filterFiles(EndDate, Download.FILTER_FILE_INCLUDE_END_DATE)
+
+#step 7 -> set
 
     download.flush()
--- classes/config.py
+++ classes/config.py
@@ -9,10 +9,10 @@ PLUGIN_STREAM = 0x0
 PLUGIN_PULK = 0x1
 
 class ConfigReader(object):
-    __Root = ""
+    __Root = ''
     _Textmining = {}
     _Database = {}
-    _Resources = {}
+    _Resources = []
     _Threads = 1
 
     def parseConfigFile(self, ConfigFile):
@@ -25,31 +25,31 @@ class ConfigReader(object):
         self.readMaxThreads()
 
     def readService(self, Root):
-        Node =""
+        Node =''
         Return = {}
-        Return["host"] = {}
-        Node = Root.find("cmd")
+        Return['host'] = {}
+        Node = Root.find('cmd')
         #we going on to check if we need to use something else then commandline
         if None is not Node:
-            Return["cmd"] = Node.text.strip()
-            del Return["host"]
+            Return['cmd'] = Node.text.strip()
+            del Return['host']
         else:
         #alternativ: We can say host[@protokoll@port], but we cannot exact error report
-            Node = Root.find("host")
+            Node = Root.find('host')
             if None is Node:
                 #raise a exception
                 pass
             else:
-                if "protokoll" not in Node.attrib:
+                if 'protokoll' not in Node.attrib:
                     #raise a exception
                     pass
-                elif "port" not in Node.attrib:
+                elif 'port' not in Node.attrib:
                     #raise a exception
                     pass
                 else:
-                    Return["host"]["name"] = Node.text.strip()
-                    Return["host"]["protokoll"] = Node.attrib["protokoll"].strip()
-                    Return["host"]["port"] = Node.attrib["port"].strip()
+                    Return['host']['name'] = Node.text.strip()
+                    Return['host']['protokoll'] = Node.attrib['protokoll'].strip()
+                    Return['host']['port'] = Node.attrib['port'].strip()
 
         return Return
 
@@ -63,9 +63,9 @@ class ConfigReader(object):
 
 
     def readTextmining(self):
-        Node = ""
+        Node = ''
 
-        Node = self.__Root.find("./services/textmining")
+        Node = self.__Root.find('./services/textmining')
         if None is Node:
             #throw exception if not in tagsoup
             pass
@@ -73,22 +73,22 @@ class ConfigReader(object):
             self._Textmining = self.readService(Node)
 
     def readDatabase(self):
-        Node = ""
+        Node = ''
 
-        Node = self.__Root.find("./services/database")
+        Node = self.__Root.find('./services/database')
         if None is Node:
             #throw exception if not in tagsoup
             pass
         else:
             self._Database = self.readService(Node)
-            self._Database["query"] = self.readSubNode(Node, "query")
-            self._Database["store"] = self.readSubNode(Node, "store")
+            self._Database['query'] = self.readSubNode(Node, 'query')
+            self._Database['store'] = self.readSubNode(Node, 'store')
 
     def readMaxThreads(self):
-        Node = ""
+        Node = ''
         Threads = 0
 
-        Node = self.__Root.find("./limitations/threads")
+        Node = self.__Root.find('./limitations/threads')
         if None is Node:
             #throw exception if not in tagsoup -> warning
                 pass
@@ -99,63 +99,51 @@ class ConfigReader(object):
                 pass
             self._Threads = int(Threads)
 
-    def readExcludeRules(self, Node):
-        Return = {}
-        Return["contains"] = []
-        Return["date"] = []
-        Return["ending"] = []
-        Return["pattern"] = []
+    def readRules(self, Nodes):
+        Node = ''
 
-        Nodes = Node.findall("excludeFiles")
+        Return = {}
+        Return['contains'] = []
+        Return['ending'] = []
+        Return['end_date'] = []
+        Return['pattern'] = []
+        Return['start_date'] = []
         for Node in Nodes:
-            if "flag" not in Node.attrib:
+            if 'flag' not in Node.attrib:
                 continue
-            allzweckWegwerfVariable = Node.attrib["flag"].strip()
+            Flag = Node.attrib['flag'].strip()
             #Why the hack has python no switch and case...narf
-            if "contains" == allzweckWegwerfVariable:
-                Return["contains"].append(Node.text.strip())
-            elif "date" == allzweckWegwerfVariable:
-                Return["date"].append(Node.text.strip())
-            elif "endswith" == allzweckWegwerfVariable:
-                Return["ending"].append(Node.text.strip())
-            elif "pattern" == allzweckWegwerfVariable:
-                Return["pattern"].append(Node.text.strip())
+            if 'contains' == Flag:
+                Return['contains'].append(Node.text.strip())
+            elif 'start_date' == Flag:
+                Return['start_date'].append(Node.text.strip())
+            elif 'end_date' == Flag:
+                Return['end_date'].append(Node.text.strip())
+            elif 'endswith' == Flag:
+                Return['ending'].append(Node.text.strip())
+            elif 'pattern' == Flag:
+                Return['pattern'].append(Node.text.strip())
             else:
+            #wir könnten ne Warunung ausgeben
                 pass
         return Return
+
+    def readExcludeRules(self, Node):
+        Nodes = []
+
+        Nodes = Node.findall('excludeFiles')
+        return self.readRules(Nodes)
 
     def readIncludeRules(self, Node):
         Nodes = []
-        allzweckWegwerfVariable = ""
-        Return = {}
-        Return["contains"] = []
-        Return["date"] = []
-        Return["ending"] = []
-        Return["pattern"] = []
 
-        Nodes = Node.findall("includeFiles")
-        for Node in Nodes:
-            if "flag" not in Node.attrib:
-                continue
-            allzweckWegwerfVariable = Node.attrib["flag"].strip()
-        #Why the hack has python no switch and case...narf
-            if "contains" == allzweckWegwerfVariable:
-                Return["contains"].append(Node.text.strip())
-            elif "date" == allzweckWegwerfVariable:
-                Return["date"].append(Node.text.strip())
-            elif "endswith" == allzweckWegwerfVariable:
-                Return["ending"].append(Node.text.strip())
-            elif "pattern" == allzweckWegwerfVariable:
-                Return["pattern"].append(Node.text.strip())
-            else:
-                pass
-
-        return Return
+        Nodes = Node.findall('includeFiles')
+        return self.readRules(Nodes)
 
     def readResourceRules(self, Node):
         Return = {}
-        Return["exclusions"] = self.readExcludeRules(Node)
-        Return["inclusions"] = self.readIncludeRules(Node)
+        Return['exclusions'] = self.readExcludeRules(Node)
+        Return['inclusions'] = self.readIncludeRules(Node)
         return Return
 
     def readSubFolder(self, Node):
@@ -165,18 +153,18 @@ class ConfigReader(object):
         #Exception
             pass
         else:
-            Return["name"] = allzweckWegwerfVariable
-            if "onInitializion" in Node.attrib:
-                Return["onInitializion"] = 1
+            Return['name'] = allzweckWegwerfVariable
+            if 'onInitializion' in Node.attrib:
+                Return['onInitializion'] = 1
             else:
-                Return["onInitializion"] = 0
+                Return['onInitializion'] = 0
 
         return Return
 
     def readSubFolders(self, Node):
         Return = []
         Nodes = []
-        Nodes = Node.findall("./subFolder")
+        Nodes = Node.findall('./subFolder')
         if not Node:
             #Exception
             pass
@@ -188,64 +176,64 @@ class ConfigReader(object):
 
     def readResource(self, Node):
         Return = {}
-        Return["folders"] = []
-        Return["rules"] = {}
-        Nodes = ""
-        allzweckWegwerfVariable = ""
+        Return['folders'] = []
+        Return['rules'] = {}
+        Nodes = ''
+        allzweckWegwerfVariable = ''
 
-        if "domain" not in Node.attrib:
+        if 'domain' not in Node.attrib:
         #raise a exception
             pass
         else:
-            allzweckWegwerfVariable = Node.attrib["domain"].strip()
+            allzweckWegwerfVariable = Node.attrib['domain'].strip()
             if not allzweckWegwerfVariable:
                 #Exception
                 pass
             else:
-                Return["address"] = allzweckWegwerfVariable
+                Return['address'] = allzweckWegwerfVariable
 
 
-        if "plugin" in Node.attrib:
-            if os.path.isfile("./plugin/" + Return["address"] + "/plugin.py"):
-                if "pulk" == Node.attrib["plugin"].text.strip():
-                    Return["plugin"] = PLUGIN_PULK
+        if 'plugin' in Node.attrib:
+            if os.path.isfile('./plugin/' + Return['address'] + '/plugin.py'):
+                if 'pulk' == Node.attrib['plugin'].text.strip():
+                    Return['plugin'] = PLUGIN_PULK
                 else:
-                    Return["plugin"] = PLUGIN_STREAM
+                    Return['plugin'] = PLUGIN_STREAM
             else:
             #raise a excption as warning
                 pass
         else:
-            Return["plugin"] = 0
+            Return['plugin'] = 0
 
-        if "xlstTransformation" in Node.attrib:
-            allzweckWegwerfVariable = Node.attrib["xlstTransformation"].strip()
+        if 'xlstTransformation' in Node.attrib:
+            allzweckWegwerfVariable = Node.attrib['xlstTransformation'].strip()
             if os.path.isfile(allzweckWegwerfVariable):
-                Return["xslt"] = allzweckWegwerfVariable
+                Return['xslt'] = allzweckWegwerfVariable
             else:
             #raise a exception
                 pass
 
-        if "md5Check" in Node.attrib:
-            Return["md5"] = 1
+        if 'md5Check' in Node.attrib:
+            Return['md5'] = 1
         else:
-            Return["md5"] = 0
+            Return['md5'] = 0
 
 
-        Return["folders"] = self.readSubFolders(Node)
-        if not Return["folders"]:
+        Return['folders'] = self.readSubFolders(Node)
+        if not Return['folders']:
             #Exceptio
             pass
-        Return["rules"] = self.readResourceRules(Node)
+        Return['rules'] = self.readResourceRules(Node)
         return Return
 
 
     def readResources(self):
-        Nodes = ""
+        Nodes = ''
 
-        Nodes = self.__Root.findall("./resourceCollection/resource")
+        Nodes = self.__Root.findall('./resourceCollection/resource')
         if not Nodes:
         #raise Exception
             pass
         else:
             for Node in Nodes:
-                self._Resources = self.readResource(Node)
+                self._Resources.append(self.readResource(Node))
