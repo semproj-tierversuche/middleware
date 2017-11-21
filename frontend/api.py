11c77ab11162d4940b3be3fdba0aba459b6bde3e
Sebastian Fischer <fischer.sebastian@gmail.com>
2017-11-21 20:00:37 +0100

    Add REST API for web frontend
    Based on the flask and flask_restful python packages.
    Currently it receives GET requests at /results/<pmid>
    and returns the JSON file data/results/<pmid>.json if it exists.

---
+++ frontend/api.py
@@ -0,0 +1,37 @@
+import json
+from pathlib import Path
+
+from flask import Flask
+from flask_restful import abort, Api, Resource
+
+# load (example) results from file
+def results_for_pmid(pmid):
+    # make sure that we only use ints when constructing the file name
+    pmid = int(pmid)
+    file_path = Path("data/results", str(pmid) + ".json")
+    if not file_path.is_file():
+        return False
+    with file_path.open() as f:
+        return json.load(f)
+
+# Flask and flask_restful setup
+
+app = Flask(__name__)
+app.config["ERROR_404_HELP"] = False
+api = Api(app)
+
+# API definition
+
+class ResultsAPI(Resource):
+    def get(self, id):
+        results = results_for_pmid(id)
+        if not results:
+            abort(404, message="No results available for this id.")
+        return results
+
+api.add_resource(ResultsAPI, '/results/<int:id>')
+
+# Run the flask app
+
+if __name__ == '__main__':
+    app.run(debug=True)
