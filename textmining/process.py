8d4b2ab56ff1c270b8d2d23532db240f601dd5ec
Sebastian Fischer <fischer.sebastian@gmail.com>
2018-01-31 00:44:07 +0100

    Remove absolute sys.path

--- textmining/process.py
+++ textmining/process.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import sys
 
-sys.path.insert(0, "/home/sefischer/.local/lib64/python3.5/site-packages")
-
 from document import Document, DocumentModel
 
 xml = sys.stdin.read()
