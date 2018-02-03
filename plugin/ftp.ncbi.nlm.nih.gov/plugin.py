3d7820ade672ba5840d44007e87a85e552a1c03e
bitPogo <geislemx@informatik.hu-berlin.de>
2018-02-03 01:37:32 +0100

    Just a safety Commit

--- plugin/ftp.ncbi.nlm.nih.gov/plugin.py
+++ plugin/ftp.ncbi.nlm.nih.gov/plugin.py
@@ -5,7 +5,36 @@ from classes.plugin_interface import PlugInInterface
 class Plugin(PlugInInterface):
 
     @staticmethod
-    def preTextminingHook(File, OriginalPath):
+    def getNonBioCFields(File, OriginalPath):
+        Documents = []
+        NonBioCInformations = []
+        with open(File, 'rb') as File:
+            XML = DOM.parse(File)
+            Root = XML.getroot()
+            #taken from https://github.com/semproj-tierversuche/middleware/blob/dev/textmining/document.py
+            Nodes = Root.findall("./PubmedArticle/MedlineCitation")
+            for Article in Nodes:
+                Documents.append(DOM.fromstring("<PubmedArticleSet><PubmedArticle><MedlineCitation>" + DOM.tostring(Article).decode('utf-8') + "</MedlineCitation></PubmedArticle></PubmedArticleSet>"))
+
+                Information = {}
+                Information["PMID"] = str(int(Article.find("./PMID").text))#we have to keep it to match later with the outputting bioC
+
+                Information["Authors"] = []
+                for Autor in Article.findall("./AuthorList/Author"):
+                    Information["Authors"].append("{}, {}".format(Author.find("LastName").text, Author.find("ForeName").text))
+
+                if Article.find("./Journal/Title"):
+                    Information["Journal"] = Article.find("./Journal/Title").text
+
+                Information["Link"] = "https://www.ncbi.nlm.nih.gov/pubmed/" + Information["PMID"]
+                if Article.findall("./KeywordList/Keyword"):
+                    Information["Keywords"] = [Element.text  for Element in Article.findall("./KeywordList/Keyword")]
+                NonBioCInformations.append(Information)
+            return (Documents, NonBioCInformations)
+
+
+    @staticmethod
+    def preTextminingHook(XML, OriginalPath):
         with open(File, 'rb') as File:
             XML = DOM.parse(File)
             BioCs = []
@@ -20,7 +49,6 @@ class Plugin(PlugInInterface):
             #taken from https://github.com/semproj-tierversuche/middleware/blob/dev/textmining/document.py
             Root = XML.getroot()
             Nodes = Root.findall("./PubmedArticle/MedlineCitation")
-            print(Nodes)
             NonBioCInformations = []
             for Article in Nodes:
                 Information = {}
@@ -40,5 +68,5 @@ class Plugin(PlugInInterface):
 
 
     @staticmethod
-    def preDatabaseHook(BioC):
+    def preDatabaseHook(BioC, OriginalPath):
         return BioC
